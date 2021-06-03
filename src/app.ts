3b7be0fe6ef8198abfdacb9900c476ef7e4eebe0
DaryaYa <lyalyaka@mail.ru>
2021-06-03 12:35:54 +0300

    init ts

---
+++ src/app.ts
@@ -0,0 +1,42 @@
+import express, { Request, Response, NextFunction } from 'express';
+const swaggerUI = require('swagger-ui-express');
+const path = require('path');
+const YAML = require('yamljs');
+const userRouter = require('./resources/users/user.router');
+const boardRouter = require('./resources/boards/board.router');
+const taskRouter = require('./resources/tasks/task.router');
+
+const app = express();
+const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
+
+console.log("Hello!");
+
+app.use(express.json());
+
+app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+app.use('/users', userRouter);
+app.use('/boards/:boardId/tasks', taskRouter);
+app.use('/boards', boardRouter);
+// app.use('/boards', taskRouter);
+// boardRouter.use('/:id/tasks', taskRouter);
+
+
+app.use('/', (req: Request, res: Response, next: NextFunction) => {
+  if (req.originalUrl === '/') {
+    res.send('Service is running!');
+    return;
+  }
+  next();
+});
+
+
+
+// app.use((err, req, res, next) => {
+//   console.error(err.stack);
+  
+//   res.status(500).send('Something went wrong!')
+// next(err);
+// });
+
+module.exports = app;
